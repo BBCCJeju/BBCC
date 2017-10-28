a55bc6580c15740758a632e48fa9ab5882845b30
phenol94 <phenolate93@gmail.com>
2017-10-28 18:26:17 +0900

    add v-card tag test

--- template/src/components/Order.vue
+++ template/src/components/Order.vue
@@ -1,13 +1,27 @@
 <template lang="html">
   <div>
       <h1>주문 폼 화면</h1>
+      <v-card>
+        <div class="v-card-image">
+            <img src="http://materializecss.com/images/sample-1.jpg">
+            <span class="v-card-title">v-card Title</span>
+        </div>
+        <div class="v-card-content">
+            <p>
+                I am a very simple v-card. I am good at containing small bits of information.
+            </p>
+        </div>
+        <div class="v-card-action">
+            <a href="#">This is a link</a>
+        </div>
+    </v-card>
   </div>
 </template>
 
 <script>
 export default {
   methods: {
-    
+
   }
 }
 </script>
