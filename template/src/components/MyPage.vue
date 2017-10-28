fc3f592f3341a27b53a90eb71755404ab1265d77
phenol94 <phenolate93@gmail.com>
2017-10-28 23:54:55 +0900

    add my-page assets

--- template/src/components/MyPage.vue
+++ template/src/components/MyPage.vue
@@ -1,14 +1,58 @@
 <template lang="html">
 <div>
 	<div class="c-box">
-	<v-card>
+	<v-card style="overflow: hidden;">
         <div class="card-content">
-            <p>
-                I am a very simple v-card. I am good at containing small bits of information.
-            </p>
-        </div>
-        <div class="card-action">
-            <a href="#">This is a link</a>
+        	<div>
+        		<div>
+	            	<img src="../assets/logo.png">
+	        	</div>
+
+	        	<div>
+		        	<div class="test1">
+		            	<p>My Name</p>
+		        	</div>
+		        	<div class="test1">
+		            	<p>My Email</p>
+		        	</div>
+		        </div>
+		    </div>
+
+        	<div>
+        		<div class="test2">
+		        	<div>
+		            	<p>전공</p>
+		        	</div>
+		        	<div>
+		        		<!-- Todo -->
+		        	</div>
+		        </div>
+			    <div class="test2">
+		        	<div>
+		            	<p>학교</p>
+		        	</div>
+		        	<div>
+		        		<!-- Todo -->
+		        	</div>
+	        	</div>
+	        	<div class="test2">
+		        	<div>
+		            	<p>구독 중인 플랜</p>
+		        	</div>
+		        	<div>
+		        		<!-- Todo -->
+		        	</div>
+		        </div>
+	        </div>
+
+	        <div>
+	        	<div class="test3">
+	            	<p>도서대여정보</p>
+	        	</div>
+	        	<div>
+	            	<img src="../assets/history-table.png" height="334" width="847">
+	        	</div>
+	        </div>
         </div>
     </v-card>
 	</div>
@@ -27,6 +71,18 @@ export default {
 
 .c-box {
 	margin: 50px;
+}
+
+.test1 {
+	margin: 20px;
+}
+
+.test2 {
+	margin: 20px;
+}
+
+.test3 {
+	margin: 20px;
 }
 
 </style>
